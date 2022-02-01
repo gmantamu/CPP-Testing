049cdae57c9826d324fd5ae65001d3de0e38a053
gmantamu <gwmandrona@gmail.com>
2022-02-01 13:27:33 -0600

    added iterator loop

--- IteratorPractice/IteratorExamples.cpp
+++ IteratorPractice/IteratorExamples.cpp
@@ -8,7 +8,14 @@ int main() {
         std::cout << testvec[i] << std::endl;
     }
 
+    //slightly cleaner code, but it still is limited in the type of lists that if can iterate through
     for(int value : testvec) {
         std::cout << value << std::endl;
     }
+
+    //        defining the iterator                  making the loop run until this
+    for(std::vector<int>::iterator it = testvec.begin(); it != testvec.end(); it++) {
+        std::cout << *it << std::endl;
+    }
+    
 }
